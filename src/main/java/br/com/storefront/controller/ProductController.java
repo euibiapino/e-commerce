60be1e7c1f8ba2c361e8a9e7379db25bee157d40
João Vitor Ibiapino <ibiapinojoaovitor@gmail.com>
2025-10-05 20:41:07 -0300

    devtools

---
+++ src/main/java/br/com/storefront/controller/ProductController.java
@@ -0,0 +1,20 @@
+package br.com.storefront.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+@AllArgsConstructor
+@Log4j2
+public class ProductController {
+
+    @GetMapping("test")
+    void teste(){
+        log.info("test 2");
+    }
+
+}
